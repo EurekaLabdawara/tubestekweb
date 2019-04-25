f4f7961bd465740e7b904b862f153125c2c726f5
henokhvita <henokhvita@gmail.com>
2019-04-25 15:08:57 +0700

    add navbar

--- application/views/landingpage.php
+++ application/views/landingpage.php
@@ -9,6 +9,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/button_inverse.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
@@ -22,7 +23,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 </head>
 
 <body>
-
+	<?php
+	include_once("navbar.php");
+	?>
     <div class="container mt-3">
         <h2>Activate Carousel with JavaScript</h2>
         <div id="myCarousel" class="carousel slide">
