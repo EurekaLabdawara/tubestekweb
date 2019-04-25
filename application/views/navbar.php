f235cc81149f0d95cd1a8d8ec442fb3e14a81004
henokhvita <henokhvita@gmail.com>
2019-04-25 23:55:43 +0700

    Navbar done, Item Landpage done

--- application/views/navbar.php
+++ application/views/navbar.php
@@ -1,36 +1,43 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-success">
-  <a class="navbar-brand" href="#">Navbar</a>
+<style>
+	.btn-outline-success{
+		background-color:white;
+	}
+</style>
+
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #d1ff99;>
+
+<a class="navbar-brand" style="background-color:transparent" href="#">
+	<img src="logo.png" class="d-inline-block align-center img-fluid"  width="100" height="100" alt="customplace">
+	 </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
+    <ul class="navbar-nav my-2 my-sm-2 mx-5">
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+          Kategori
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+          <a id="btnKatCaseHP" class="dropdown-item">Case Handphone</a>
+          <a id="btnKatBaju" class="dropdown-item">Custom Baju</a>
         </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+
+
+
+    <form class="form-inline my-2 my-lg-0" style="width:65%">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" style="width:80%">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
-	<a class="btn btn-outline-success btn-inverse my-2 my-sm-0 mx-2" href="#">Login / Register</a>
+	<a class="btn btn-outline-success my-2 my-sm-0 mx-1" href="#">
+	<i class="fas fa-shopping-cart"></i>
+	 </a>
+	<a class="btn btn-outline-success my-2 my-sm-0 mx-5" style="float:right;" href="#">Login / Register</a>
+
   </div>
+
 </nav>
+
