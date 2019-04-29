c26de2ef1cf10de1c0d1f937857ef8a6dd86dc6d
Eureka Labdawara <eurekalabdawara@gmail.com>
2019-04-29 13:03:25 +0700

    Add Views

---
+++ application/views/Vendor/dashboard.php
@@ -0,0 +1,151 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>CustomPlace</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?> assets/css/button_inverse.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <style>
+    /* Make the image fully responsive */
+
+    div.item p {
+        text-align: center;
+    }
+
+    div.item img {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    </style>
+</head>
+
+<body>
+    <?php
+    // include_once("navbar_vendor.php");
+    ?>
+    <div class="wrapper">
+        <div class="card">
+            <img src="bumper-iphone.png" class="card-img-top" alt="Bumper Iphone">
+            <div class="card-body">
+                <h5 class="card-title">Case Bumper Iphone</h5>
+                <p class="card-text">Case bumper merupakan case dengan desain simple mengelilingi sisi samping
+                    gadget, sehingga desain asli gadget tetap terlihat.</p>
+            </div>
+            <div class="card-footer">
+                <a href="#" class="card-link">Yuk Custom..</a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="wooden sony.png" class="card-img-top" alt="Wooden Sony">
+            <div class="card-body">
+                <h5 class="card-title">Case Wooden Sony</h5>
+                <p class="card-text">Case Wooden merupakan case dari bahan kayu asli yang didalamnya terdapat
+                    bantalan halus untuk melindungi handphone.</p>
+            </div>
+            <div class="card-footer">
+                <a href="#" class="card-link">Yuk Custom..</a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="flip lenovo hitam.png" class="card-img-top" alt="Flip lenovo">
+            <div class="card-body">
+                <h5 class="card-title">Case Flip Lenovo</h5>
+                <p class="card-text">Case Flip merupakan case yang menutup bagian belakang sekaligus bagian depan
+                    sehingga berperan sebagai pelindung layar.</p>
+            </div>
+            <div class="card-footer">
+                <a href="#" class="card-link">Yuk Custom..</a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="bumper-iphone.png" class="card-img-top" alt="Bumper Iphone">
+            <div class="card-body">
+                <h5 class="card-title">Case Bumper Iphone</h5>
+                <p class="card-text">Case bumper merupakan case dengan desain simple mengelilingi sisi samping
+                    gadget, sehingga desain asli gadget tetap terlihat.</p>
+            </div>
+            <div class="card-footer">
+                <a href="#" class="card-link">Yuk Custom..</a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="wooden sony.png" class="card-img-top" alt="Wooden Sony">
+            <div class="card-body">
+                <h5 class="card-title">Case Wooden Sony</h5>
+                <p class="card-text">Case Wooden merupakan case dari bahan kayu asli yang didalamnya terdapat
+                    bantalan halus untuk melindungi handphone.</p>
+            </div>
+            <div class="card-footer">
+                <a href="#" class="card-link">Yuk Custom..</a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="1depan.png" class="card-img-top" alt="Bumper Iphone">
+            <div class="card-body">
+                <h5 class="card-title">Case Bumper Iphone</h5>
+                <p class="card-text">Case bumper merupakan case dengan desain simple mengelilingi sisi samping
+                    gadget, sehingga desain asli gadget tetap terlihat.</p>
+            </div>
+            <div class="card-footer">
+                <a href="#" class="card-link">Yuk Custom..</a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="2depan.png" class="card-img-top" alt="Wooden Sony">
+            <div class="card-body">
+                <h5 class="card-title">Case Wooden Sony</h5>
+                <p class="card-text">Case Wooden merupakan case dari bahan kayu asli yang didalamnya terdapat
+                    bantalan halus untuk melindungi handphone.</p>
+            </div>
+            <div class="card-footer">
+                <a href="#" class="card-link">Yuk Custom..</a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="3depan.png" class="card-img-top" alt="Flip lenovo">
+            <div class="card-body">
+                <h5 class="card-title">Case Flip Lenovo</h5>
+                <p class="card-text">Case Flip merupakan case yang menutup bagian belakang sekaligus bagian depan
+                    sehingga berperan sebagai pelindung layar.</p>
+            </div>
+            <div class="card-footer">
+                <a href="#" class="card-link">Yuk Custom..</a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="4depan.png" class="card-img-top" alt="Bumper Iphone">
+            <div class="card-body">
+                <h5 class="card-title">Case Bumper Iphone</h5>
+                <p class="card-text">Case bumper merupakan case dengan desain simple mengelilingi sisi samping
+                    gadget, sehingga desain asli gadget tetap terlihat.</p>
+            </div>
+            <div class="card-footer">
+                <a href="#" class="card-link">Yuk Custom..</a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="5depan.png" class="card-img-top" alt="Wooden Sony">
+            <div class="card-body">
+                <h5 class="card-title">Case Wooden Sony</h5>
+                <p class="card-text">Case Wooden merupakan case dari bahan kayu asli yang didalamnya terdapat
+                    bantalan halus untuk melindungi handphone.</p>
+            </div>
+            <div class="card-footer">
+                <a href="#" class="card-link">Yuk Custom..</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
